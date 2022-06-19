ea30e99a5c774363c5255fb10879b04836df6406
andriusmw <andriusmw@yahoo.es>
2022-06-19 10:56:21 +0200

    conditional render para usar menu nav

--- fotoapp/src/App.js
+++ fotoapp/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import {SearchPhotos} from "./pages/SearchPhotos"
 import { useState, useEffect } from 'react';
+import {MyPhotos} from "./pages/Myphotos"
 
 function App() {
   const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
   const [favorites, setFavorites] = useState(storedFavorites);
+  const [showingPage, setShowingPage] = useState("search-photos");
 
   console.log(storedFavorites)  ;
 
@@ -29,13 +31,20 @@ useEffect(() => {
         <h1>Photo Catalog</h1>
         <nav>
           <ul>
-            <li>My Photos</li>
-            <li>Search new photos</li>
+            <li><button onClick={() => {
+              setShowingPage("my-photos")
+            }} >My Photos</button></li>
+
+            <li><button onClick={() => {
+              setShowingPage("search-photos")
+            }} >Search new photos</button></li>
           </ul>
         </nav>
       </header>
       <main>
-          <SearchPhotos addToFavorites={addToFavorites} ></SearchPhotos>
+         {showingPage === "search-photos" && ( <SearchPhotos addToFavorites={addToFavorites} /> )}
+         {showingPage === "my-photos" && (  <MyPhotos/> )}
+         
       </main>
       <footer>
         <p>Hack a Boss 2022@</p>
---
+++ fotoapp/src/pages/Myphotos.js
@@ -0,0 +1,7 @@
+export const MyPhotos = () => {
+    return (
+    <section>
+        <h2>MyPhotos</h2>
+    </section>
+            )
+}
