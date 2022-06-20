3ef8845ff3e5791f12e796c7019f48c383411735
andriusmw <andriusmw@yahoo.es>
2022-06-20 10:37:16 +0200

    env estaba fuera de raiz. falta borrar de favoritos,todo lo demas va bien

--- fotoapp/src/components/SearchPhotosForm.js
+++ fotoapp/src/components/SearchPhotosForm.js
@@ -4,8 +4,9 @@ export const SearchPhotosForm = ({setSearchResults2}) => {
             event.preventDefault();
           
           const query = event.target.elements.search.value;
+          console.log(process.env.REACT_APP_API_KEY)
 
-          const res = await  fetch(`https://api.pexels.com/v1/search?query=${query}&locale=es-ES&per_page=80`,
+          const res = await fetch(`https://api.pexels.com/v1/search?query=${query}&locale=es-ES&per_page=80`,
             {
                 headers: {
                     Authorization: process.env.REACT_APP_API_KEY,
@@ -14,11 +15,12 @@ export const SearchPhotosForm = ({setSearchResults2}) => {
            );
 
             console.log(res)
+            
             //el fetch devuelve un error 500 y por eso la respuesta no se puede parsear a json porque no nos devuelve un json con un
             //array, sino que nos devuelve una respuesta. ¿problema del servidor? la llamada antes iba bien, de hecho se realiza y nos da
             //una respuesta detipo 500 que es error interno del servior o que ha roto por pedirle algo para lo que no está preparado,
             //pero antes iba.
-            console.log(res.responseText)
+            //console.log(res.responseText)
             const results = await res.json()
             console.log(results);
             setSearchResults2(results.photos) //cambia el estado con los resultados de la búsqueda
